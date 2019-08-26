3181d479da50c641f9c4a45e4c5ec0bda23b4a46
Gavin <gavin.leavitt@gmail.com>
2019-08-26 14:37:56 -0600

    Began work on movement

---
+++ Game_DGM_2670_Fall_2019/Assets/Scripts/ControlInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class ControlInput : MonoBehaviour
+{
+    public float speed = 1.0f;
+    //private float AxisAngle;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //AxisAngle = Vector2.Angle(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        // (FIX SO DIAGONAL MOVEMENT IS NOT FASTER THAN HORIZ/VERT)
+        transform.Translate(Input.GetAxis("Horizontal")*speed, Input.GetAxis("Vertical")*speed, 0);
+    }
+}

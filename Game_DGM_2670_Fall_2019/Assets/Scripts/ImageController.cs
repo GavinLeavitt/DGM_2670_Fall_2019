c3cef5146116bc4a73bcfd9f78698428420749b2
Gavin <gavin.leavitt@gmail.com>
2019-09-16 14:48:27 -0600

    Created start event for ImageUpdate script

--- Game_DGM_2670_Fall_2019/Assets/Scripts/ImageController.cs
+++ Game_DGM_2670_Fall_2019/Assets/Scripts/ImageController.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Image))]
 public class ImageController : MonoBehaviour
 {
+    public UnityEvent startEvent;
     private Image imageComponent;
     private void Start()
     {
         imageComponent = GetComponent<Image>();
+        startEvent.Invoke();
     }
     
     public void UpdateImageComponent(FloatData dataObj)

0d03f76a2a494fb068261bd1b9473eba289c048d
Gavin <gavin.leavitt@gmail.com>
2019-12-11 14:40:38 -0700

    Started page turn work

---
+++ Tower's Tale/Assets/Scripts/OnClickActionInvoke.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickActionInvoke : MonoBehaviour
+{
+    public GameAction gameActionObj;
+    private void OnMouseDown()
+    {
+        gameActionObj.Raise();
+        Debug.Log("CLICK HAPPEN");
+    }
+}
